b2fd7ff0a2ffdbf808ec0c0d1b0006d05ebc844f
Renan <renanv@ipiranga.com.br>
2019-09-03 14:56:07 -0300

    delete lib folder

--- lib/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Tracer, TracerConfig, TracerExceptions, ITracerExceptions } from './Tracer';
-import Span from './Span';
-import { Tags } from 'opentracing';
-export { Tracer, Tags, Span, TracerConfig, TracerExceptions, ITracerExceptions };
